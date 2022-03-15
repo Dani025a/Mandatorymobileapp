922c14c2bd88e1dae8301e57c3713e519a9ade62
dani025a <dani025a@edu.easj.dk>
2022-03-15 13:43:43 +0100

    Login and register added

--- app/src/main/java/com/example/mandatorymobileapp/Repository/AuthAppRepository.kt
+++ app/src/main/java/com/example/mandatorymobileapp/Repository/AuthAppRepository.kt
@@ -1,35 +1,66 @@
 package com.example.mandatorymobileapp.Repository
-
 import android.app.Application
+import android.os.Build
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
 
-class AuthAppRepository {
-    private var application: Application? = null
+class AuthAppRepository(private val application: Application) {
+    private val firebaseAuth: FirebaseAuth
+    val userLiveData: MutableLiveData<FirebaseUser?>
+    val loggedOutLiveData: MutableLiveData<Boolean>
 
-    private var firebaseAuth: FirebaseAuth? = null
-    private var userLiveData: MutableLiveData<FirebaseUser?>? = null
-    private var loggedOutLiveData: MutableLiveData<Boolean>? = null
+    @RequiresApi(Build.VERSION_CODES.P)
+    fun login(email: String?, password: String?) {
+        firebaseAuth.signInWithEmailAndPassword(email!!, password!!)
+            .addOnCompleteListener(
+                application.mainExecutor
+            ) { task ->
+                if (task.isSuccessful) {
+                    userLiveData.postValue(firebaseAuth.currentUser)
+                } else {
+                    Toast.makeText(
+                        application.applicationContext,
+                        "Login Failure: " + task.exception!!.message,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
 
-    fun AuthAppRepository(application: Application?) {
-        this.application = application
+    @RequiresApi(Build.VERSION_CODES.P)
+    fun register(email: String?, password: String?) {
+        firebaseAuth.createUserWithEmailAndPassword(email!!, password!!)
+            .addOnCompleteListener(
+                application.mainExecutor
+            ) { task ->
+                if (task.isSuccessful) {
+                    userLiveData.postValue(firebaseAuth.currentUser)
+                } else {
+                    Toast.makeText(
+                        application.applicationContext,
+                        "Registration Failure: " + task.exception!!.message,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+    fun logOut() {
+        firebaseAuth.signOut()
+        loggedOutLiveData.postValue(true)
+    }
+
+    init {
         firebaseAuth = FirebaseAuth.getInstance()
         userLiveData = MutableLiveData()
         loggedOutLiveData = MutableLiveData()
-        if (firebaseAuth!!.currentUser != null) {
-            userLiveData!!.postValue(firebaseAuth!!.currentUser)
-            loggedOutLiveData!!.postValue(false)
+        if (firebaseAuth.currentUser != null) {
+            userLiveData.postValue(firebaseAuth.currentUser)
+            loggedOutLiveData.postValue(false)
         }
     }
-
-    fun getUserLiveData(): MutableLiveData<FirebaseUser?>? {
-        return userLiveData
-    }
-
-    fun getLoggedOutLiveData(): MutableLiveData<Boolean>? {
-        return loggedOutLiveData
-    }
-
 }
